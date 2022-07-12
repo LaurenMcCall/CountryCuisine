8cb6308de607dc0dbe85c5a0ac74bfe676262f1f
Lauren McCall <lmmccolloch@gmail.com>
2022-07-12 17:44:28 -0400

    update scripts in package.json

--- Models/Country.cs
+++ Models/Country.cs
@@ -8,19 +8,19 @@ namespace CountryCuisine.Models
     {
         public int Id { get; set; }
         public DateTime? DateAdded { get; set; } = DateTime.Now;
-       
-       [Required(ErrorMessage = "You must provide a country name. ")]
+
+        [Required(ErrorMessage = "You must provide a country name. ")]
         public string Name { get; set; }
 
-       [Required(ErrorMessage = "You must provide a flag image url. ")]
+        [Required(ErrorMessage = "You must provide a flag image url. ")]
         public string FlagUrl { get; set; }
 
         public List<Recipe> Recipes { get; set; }
         public List<Movie> Movies { get; set; }
         public List<Music> Musics { get; set; }
 
-        public int UserId {get; set; }
+        public int UserId { get; set; }
 
-        public User User {get; set; }
+        public User User { get; set; }
     }
 }

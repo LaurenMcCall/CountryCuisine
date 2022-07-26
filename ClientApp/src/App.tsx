a2363a304b82373960c9fea88edaae4b15d6f883
Lauren McCall <lmmccolloch@gmail.com>
2022-07-26 12:56:17 -0400

    added randomized country button to nav bar

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Route, Routes } from 'react-router'
+import React, { useEffect, useState } from 'react'
+import { Route, Routes, useNavigate } from 'react-router'
 import { Link } from 'react-router-dom'
 
 import { Countries } from './pages/Countries'
@@ -14,6 +14,7 @@ import { Footer } from './components/Footer'
 import { AddRecipe } from './pages/AddRecipe'
 import { AddMusic } from './pages/AddMusic'
 import { AddMovie } from './pages/AddMovie'
+import { CountryType } from './types'
 // import { AddUserImage } from './pages/AddUserImage'
 
 export function App() {
@@ -29,13 +30,9 @@ export function App() {
           <Route path="/countries/:id/addMusic" element={<AddMusic />} />
           <Route path="/countries/:id/addMovie" element={<AddMovie />} />
           <Route path="/countries/:id" element={<Country />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/signupCountryCuisineNight7335" element={<SignUp />} />
+          <Route path="/loginCountryCuisineNight7335" element={<Login />} />
           <Route path="/passport/:id" element={<Passport />} />
-          {/* <Route
-            path="/passport/:id/addPassportPhoto"
-            element={<AddUserImage />}
-          /> */}
           <Route path="*" element={'No Match'} />
         </Routes>
       </main>
@@ -46,6 +43,45 @@ export function App() {
 
 function LoggedInNav() {
   const user = getUser()
+  const history = useNavigate()
+  const [countries, setCountries] = useState<CountryType[]>([])
+
+  useEffect(() => {
+    const loadCountries = () => {
+      fetch(`/api/countries/`)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data)
+
+          setCountries(data)
+        })
+    }
+    loadCountries()
+  }, [])
+
+  const randomCountryIds: Array<Number | undefined> = []
+
+  function getRandomCountryId() {
+    countries.forEach((country) => {
+      randomCountryIds.push(country.id)
+    })
+  }
+  getRandomCountryId()
+
+  async function loadOneCountry() {
+    var randomCountryId =
+      randomCountryIds[Math.floor(Math.random() * randomCountryIds.length)]
+    const response = await fetch('/api/countries/' + randomCountryId)
+
+    if (response.ok) {
+      console.log(response)
+      history('/countries/' + randomCountryId)
+      return response.json()
+    } else {
+      throw await response.json()
+    }
+  }
+
   function handleLogout() {
     logout()
 
@@ -66,6 +102,13 @@ function LoggedInNav() {
               title="search for a country"
               href="/search"
             ></a>
+          </li>
+          <li>
+            <button
+              onClick={loadOneCountry}
+              className="fa-solid fa-shuffle nav-icon"
+              title="go to a random country"
+            ></button>
           </li>
           <li>
             <a
@@ -101,6 +144,44 @@ function LoggedInNav() {
 }
 
 function SignedOutNav() {
+  const history = useNavigate()
+  const [countries, setCountries] = useState<CountryType[]>([])
+
+  useEffect(() => {
+    const loadCountries = () => {
+      fetch(`/api/countries/`)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data)
+
+          setCountries(data)
+        })
+    }
+    loadCountries()
+  }, [])
+
+  const randomCountryIds: Array<Number | undefined> = []
+
+  function getRandomCountryId() {
+    countries.forEach((country) => {
+      randomCountryIds.push(country.id)
+    })
+  }
+  getRandomCountryId()
+
+  async function loadOneCountry() {
+    var randomCountryId =
+      randomCountryIds[Math.floor(Math.random() * randomCountryIds.length)]
+    const response = await fetch('/api/countries/' + randomCountryId)
+
+    if (response.ok) {
+      console.log(response)
+      history('/countries/' + randomCountryId)
+      return response.json()
+    } else {
+      throw await response.json()
+    }
+  }
   return (
     <>
       <Link to="/">
@@ -116,6 +197,13 @@ function SignedOutNav() {
               title="search for a country"
               href="/search"
             ></a>
+          </li>
+          <li>
+            <button
+              onClick={loadOneCountry}
+              className="fa-solid fa-shuffle nav-icon"
+              title="go to a random country"
+            ></button>
           </li>
           {/* <button className="nav-button">
             <Link to="/login">Log In</Link>
